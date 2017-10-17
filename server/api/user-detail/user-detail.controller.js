dccbb3346f7ff3478de4a612a6dfc4adaeba0220
timothyfranzke <Carpots3+>
2017-10-17 16:20:24 -0500

    Added user methods for vendor entities

--- server/api/user-detail/user-detail.controller.js
+++ server/api/user-detail/user-detail.controller.js
@@ -1,84 +1,124 @@
 let mongo = require('mongodb');
-
+let logging = require('../../services/logging/logging.service');
+let className = 'user-detail.controller';
 
 export function list(req,res){
-    var entity_id = req.user.application_data.entity_id;
-    console.log(entity_id);
+  let entity_id = mongo.ObjectID(req.user.application_data.entity_id);
+  let db = require('../../services/db/db.service').getDb();
 
-    let db = require('../../services/db/db.service').getDb();
-    db.collection('userDetail').find({"active":true, "entity_id":entity_id}).toArray(function(err, result){
-        if (err) return console.log(err);
+  logging.INFO(className, list.name, "entity_id : " + entity_id);
+  db.collection('entity').findOne({"active":true, "_id": entity_id })
+    .then(function(entityResult)
+    {
+      logging.INFO(className, list.name, "found entity");
+      logging.INFO(className, list.name, entityResult);
+      if(entityResult.type === 'school'){
+        logging.INFO(className, list.name, "entity type is school");
+        db.collection('userDetail').find({"active":true, "entity_id":entity_id.str}).toArray(function(err, result){
+          if (err) return console.log(err);
 
-        var userDetailResult = {
-            "data" : result
-        };
-        res.json(userDetailResult);
-    })
+          let userResult = {
+            data : result
+          };
+
+          res.json(userResult);
+        })
+      }
+      else{
+        logging.INFO(className,list.name, "entity type is vendor");
+        var userEntityIds = entityResult.associated_entities;
+        logging.INFO(className,list.name, "userEntityIds : " + userEntityIds);
+        userEntityIds.push(entity_id.str);
+        db.collection('userDetail').find({"entity_id": {$in: userEntityIds}}).toArray(function(err,result){
+          if (err) return console.log(err);
+
+          let userResult = {
+            data : result
+          };
+
+          res.json(userResult);
+        });
+      }
+    }, function(err){
+      logging.ERROR(err, className, list.name, "call to entity collection failed");
+      res.status(400);
+    });
 };
 
 export function get(req,res){
-    let db = require('../../services/db/db.service').getDb();
-    var id = mongo.ObjectID(req.params.id);
-    var entity_id = req.user.application_data.entity_id;
+  let db = require('../../services/db/db.service').getDb();
+  var id = mongo.ObjectID(req.params.id);
+  var entity_id = req.user.application_data.entity_id;
 
-    db.collection('userDetail').find( {"_id" : id, "entity_id":entity_id}).toArray(function(err,result){
-        if (err) return console.log(err);
+  db.collection('userDetail').find( {"_id" : id, "entity_id":entity_id}).toArray(function(err,result){
+    if (err) return console.log(err);
 
-        res.json(result);
-    })
+    res.json(result);
+  })
 };
 
 export function create(req,res){
-    let db = require('../../services/db/db.service').getDb();
-    let emailer = require('../../services/message/message.service');
+  let db = require('../../services/db/db.service').getDb();
+  let emailer = require('../../services/message/message.service');
 
-    req.body.active = true;
-    req.body.date_created = Date.now();
-    req.body.entity_id = req.user.application_data.entity_id;
+  req.body.active = true;
+  req.body.date_created = Date.now();
+  req.body.entity_id = req.user.application_data.entity_id;
 
-    db.collection('userDetail').insert(req.body, function(err, entityResult){
-        if (err) return console.log(err);
+  db.collection('userDetail').insert(req.body, function(err, entityResult){
+    if (err) return console.log(err);
 
-        if(!!req.body.email)
-        {
-          let message = {
-            to: req.body.email,
-            subject: "K-Spaces Account Created",
-            body:"<p>An account has been created for you using this email</p><p>Please log in at k-spaces.herokuapp.com and enter the temporary password <b>"+ req.body.temp_password +"</b></p>"
-          };
+    if(!!req.body.email)
+    {
+      let message = {
+        to: req.body.email,
+        subject: "K-Spaces Account Created",
+        body:"<p>An account has been created for you using this email</p><p>Please log in at k-spaces.herokuapp.com and enter the temporary password <b>"+ req.body.temp_password +"</b></p>"
+      };
 
-          emailer.sendEmail(message, function(err, mailResponse){
-            res.json(entityResult)
-          });
-        }
-        else{
-          res.json(entityResult);
-        }
-    })
+      try{
+        emailer.sendEmail(message, function(err, mailResponse){
+          if(!!err){
+            logging.ERROR(err, className, create.name);
+          }
+
+          res.json(entityResult)
+        });
+      }
+      catch(exception){
+        logging.ERROR(exception, className, create.name);
+        res.status(422);
+      }
+
+    }
+    else{
+      res.json(entityResult);
+    }
+  })
 };
 
 
 export function update(req, res){
-    let db = require('../../services/db/db.service').getDb();
-    var entity_id = req.user.application_data.entity_id;
-    var id = mongo.ObjectID(req.params.id);
-    delete req.body._id;
+  let db = require('../../services/db/db.service').getDb();
+  var entity_id = req.user.application_data.entity_id;
+  var id = mongo.ObjectID(req.params.id);
+  delete req.body._id;
 
-    db.collection('userDetail').findOneAndUpdate({"_id":id, "entity_id":entity_id}, {$set : req.body}, function(err, result){
-        if (err) return console.log(err);
+  db.collection('userDetail').findOneAndUpdate({"_id":id, "entity_id":entity_id}, {$set : req.body}, function(err, result){
+    if (err) return console.log(err);
 
-        res.json(result);
-    })
+    res.json(result);
+  })
 };
 
 export function remove(req, res){
-    let db = require('../../services/db/db.service').getDb();
-    var id = mongo.ObjectID(req.params.id);
-    var entity_id = req.user.application_data.entity_id;
+  let db = require('../../services/db/db.service').getDb();
+  var id = mongo.ObjectID(req.params.id);
+  var entity_id = req.user.application_data.entity_id;
 
-    db.collection('userDetail').findOneAndUpdate({"_id":id, "entity_id":entity_id}, {$set : {"active":false}}, function(err, result){
-        if (err) return console.log(err);
+  db.collection('userDetail').findOneAndUpdate({"_id":id, "entity_id":entity_id}, {$set : {"active":false}}, function(err, result){
+    if (err) return console.log(err);
 
-        res.json(result);
-    })
+    res.json(result);
+  })
 };
